2bf8ec748aeade5341c6487caaf769c25b53babf
aagneye-syam <saagneye2003@gmail.com>
2024-02-22 23:29:40 +0530

    react-router-dom used

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout />,
+    errorElement:<ErrorPage />,
+    children:[{}]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
